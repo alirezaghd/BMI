f05fd887b216a0ea23f62d910fa0039535626d29
alirezaghd <45558196+alirezaghd@users.noreply.github.com>
2021-03-14 16:48:30 +0330

    Add files via upload

--- BMI.py
+++ BMI.py
@@ -1,15 +1,20 @@
 
-w =int(input("Insert Wight"))
-h = int(input("Insert Hight"))
+w = int(input("Insert Wight "))
+h = float(input("Insert Hight 75"))
 
 bmi = w/(h*h)
 
-    if bmi <= 25 and < 30:
+if bmi < 18.5:
+        print('Your BMI is', bmi,'which means you are underweight.')
+    
+elif bmi <= 18.5 and bmi < 25:
+        print('Your BMI is', bmi,'which means you are normal.')
+
+elif bmi <= 25 and bmi < 30:
         print('Your BMI is', bmi,'which means you are overweight.')
 
-    elif bmi => 30 and bmi < 25:
+elif bmi >= 30 and bmi < 35:
         print('Your BMI is', bmi,'which means you are obese.')
 
-
-    elif bmi > 35:
+elif bmi > 35:
         print('Your BMI is', bmi,'which means you are Exteremely obese.')
